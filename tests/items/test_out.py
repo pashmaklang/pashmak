b36ef5658a2b71f261a2eca8c07500d40e78aaeb
parsa mpsh <parsa84sh1384@gmail.com>
2020-08-30 20:13:39 +0430

    tests: add test_out

---
+++ tests/items/test_out.py
@@ -0,0 +1,40 @@
+# test_out.py
+#
+# the pashmak project
+# Copyright 2020 parsa mpsh <parsampsh@gmail.com>
+#
+# This file is part of pashmak.
+#
+# pashmak is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pashmak is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with cati.  If not, see <https://www.gnu.org/licenses/>.
+##################################################
+
+
+from TestCore import TestCore
+
+script_content = '''
+mem 'hi there'; out ^;
+
+mem 'hello world\\n'; out ^;
+
+set %name; mem 'pashmak'; copy %name;
+
+out %name;
+'''
+
+class test_out(TestCore):
+    def run(self):
+        program_output = self.run_script(script_content)['output']
+
+        self.assert_equals(program_output , 'hi therehello world\npashmak')
+
