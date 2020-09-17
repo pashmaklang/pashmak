9a41226c83aba77e81dea4d1a98d0f152b9c81d0
parsa mpsh <parsa84sh1384@gmail.com>
2020-09-17 17:42:39 +0430

    system operation: clean up

--- src/operations/system.py
+++ src/operations/system.py
@@ -22,17 +22,14 @@
 import os
 
 def run(self , op):
-    arg = self.one_arg_required('system command gets a parameter' , op)
+    self.require_one_argument(op , 'system operation requires argument')
+    arg = op['args'][0]
+    self.arg_should_be_variable_or_mem(arg , op)
 
     if arg == '^':
         cmd = self.get_mem()
     else:
-        if arg[0] == '$':
-            try:
-                cmd = self.variables[arg[1:]]
-            except:
-                self.raise_variable_error(arg , op)
-        else:
-            self.raise_error('SyntaxError' , 'unexpected "' + arg[0] + '"' , op)
-            
+        self.variable_required(arg[1:] , op)
+        cmd = self.variables[arg[1:]]
+
     self.mem = os.system(cmd)
--- tests/items/test_system.py
+++ tests/items/test_system.py
@@ -19,16 +19,12 @@
 # along with pashmak.  If not, see <https://www.gnu.org/licenses/>.
 ##################################################
 
-
 import time, os
 
 from TestCore import TestCore
 
 script_content = '''
 mem 'start\\n'; out ^;
-
-mem 'end\\n'; out ^;
-
 mem 'touch /tmp/pashmak-test-created-file-<rand>'; system ^;
 '''
 
@@ -37,23 +33,33 @@ set $cmd; mem "touch /tmp/pashmak-test-created-file-<rand>"; copy $cmd;
 system $cmd;
 '''
 
+script_content_c = '''
+system $notfound;
+'''
+
+script_content_d = '''
+system fgfdtyjgh;
+'''
+
 class test_system(TestCore):
     def run(self):
         rand = time.time()
         program_data = self.run_script(script_content.replace('<rand>' , str(rand)))
-
-        self.assert_equals(program_data['output'] , 'start\nend\n')
-
+        self.assert_equals(program_data['output'] , 'start\n')
         self.assert_equals(program_data['mem'] , 0)
-
         self.assert_true(os.path.isfile('/tmp/pashmak-test-created-file-' + str(rand)))
         os.remove('/tmp/pashmak-test-created-file-' + str(rand))
 
-        # next script
         rand = time.time()
         program_data = self.run_script(script_content_b.replace('<rand>' , str(rand)))
         self.assert_true(os.path.isfile('/tmp/pashmak-test-created-file-' + str(rand)))
         os.remove('/tmp/pashmak-test-created-file-' + str(rand))
+
+        program_data = self.run_script(script_content_c)
+        self.assert_not_equals(program_data['runtime_error'] , None)
+
+        program_data = self.run_script(script_content_d)
+        self.assert_not_equals(program_data['runtime_error'] , None)
 
 
 
