c3cafc3acb08962300fffa852530f6781836e1c5
parsa mpsh <parsampsh@gmail.com>
2020-10-06 22:01:30 +0330

    module builder: minify output

--- src/core/modules.py
+++ src/core/modules.py
@@ -25,124 +25,100 @@ modules = {}
 
 modules["random"] = """
 namespace random;
-
     func randint;
         set $args; copy $args;
         py 'self.mem = random.randint(' + str($args[0]) + ',' + str($args[1]) + ')';
     endfunc;
-
     func random;
         py 'self.mem = random.random()';
     endfunc;
-
 endnamespace;
 """
 modules["time"] = """
 namespace time;
-
     func time;
         py 'self.mem = time.time()';
     endfunc;
-
     func sleep;
         set $tmp_time_sleep_for; copy $tmp_time_sleep_for;
         py 'self.mem = time.sleep(' + str($tmp_time_sleep_for) + ')';
     endfunc;
-
 endnamespace;
 """
 modules["file"] = """
 namespace file;
-
     func open;
         set $args; copy $args;
         mem open($args[0] , $args[1]);
         free $args;
     endfunc;
-
     func close;
         set $file; copy $file;
         mem $file.close();
         free $file;
     endfunc;
-
     func read;
         set $file; copy $file;
         mem $file.read();
         free $file;
     endfunc;
-
     func write;
         set $args; copy $args;
         mem $args[0].write($args[1]);
         free $args;
     endfunc;
-
 endnamespace;
 """
 modules["hash"] = """
 namespace hash;
-
 	func sha256;
 		set $value; copy $value;
 		py 'self.mem = hashlib.sha256("' + $value + '".encode()).hexdigest()';
 		free $value;
 	endfunc;
-
 	func md5;
 		set $value; copy $value;
 		py 'self.mem = hashlib.md5("' + $value + '".encode()).hexdigest()';
 		free $value;
 	endfunc;
-
 endnamespace;
 """
 modules["stdlib"] = """
 func print;
     out ^;
 endfunc;
-
 func import;
     include ^;
 endfunc;
-
 func exit;
     return ^;
 endfunc;
-
 func py;
     python ^;
 endfunc;
-
 func sys;
     system ^;
 endfunc;
-
 func std.chdir;
     chdir ^;
 endfunc;
-
 func std.eval;
     eval ^;
 endfunc;
-
 func endns;
     endnamespace;
 endfunc;
-
 func raise;
 	set $exdata; copy $exdata;
 	mem "self.raise_error('" + $exdata[0] + "' , '" + $exdata[1] + "' , op)";
     python ^;
     free $exdata;
 endfunc;
-
 func assert;
     gotoif tmp_assert_after_section;
     raise ['AssertError' , 'asserting that false is true'];
     section tmp_assert_after_section;
 endfunc;
-
 func gset;
 	set $args; copy $args;
 	mem 'self.variables["' + $args[0] + '"] = self.get_var("args")[1]';
