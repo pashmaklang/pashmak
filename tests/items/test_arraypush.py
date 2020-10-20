538db1a154f9d6f24d239c2d4aa2f3eadd6bf7ac
parsa mpsh <parsampsh@gmail.com>
2020-10-20 15:08:56 +0330

    Revert "update copyright headers"
    This reverts commit a14c7e0aec2d988d7f744bfcadeb09ef47cee495.

--- tests/items/test_arraypush.py
+++ tests/items/test_arraypush.py
@@ -1,24 +1,23 @@
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# test_arraypush.py															#
-#																		#
-# the pashmak project                                                   #
-# Copyright 2020 parsa mpsh <parsampsh@gmail.com>                       #
-#                                                                       #
-# This file is part of pashmak.                                         #
-#                                                                       #
-# pashmak is free software: you can redistribute it and/or modify       #
-# it under the terms of the GNU General Public License as published by  #
-# the Free Software Foundation, either version 3 of the License, or     #
-# (at your option) any later version.                                   #
-#                                                                       #
-# pashmak is distributed in the hope that it will be useful,            #
-# but WITHOUT ANY WARRANTY; without even the implied warranty of        #
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
-# GNU General Public License for more details.                          #
-#                                                                       #
-# You should have received a copy of the GNU General Public License     #
-# along with pashmak.  If not, see <https://www.gnu.org/licenses/>.     #
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# test_arraypush.py
+#
+# the pashmak project
+# Copyright 2020 parsa mpsh <parsampsh@gmail.com>
+#
+# This file is part of pashmak.
+#
+# pashmak is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# pashmak is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with pashmak.  If not, see <https://www.gnu.org/licenses/>.
 ##################################################
 
 ''' The test '''
