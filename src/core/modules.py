bde112601a2b30d8f01efa869b934756f2171eb9
parsa mpsh <parsampsh@gmail.com>
2021-01-04 13:02:14 +0330

    build modules

--- src/core/modules.py
+++ src/core/modules.py
@@ -574,7 +574,28 @@ import @sys.path
 namespace sys
     $pashmakinfo = {"version": version.version, "pythoninfo": sys.version.replace("\\n", "")}
 endns"""
-modules["sys.path"] = """namespace sys
+modules["sys.path"] = """#
+# path.pashm
+#
+# The Pashmak Project
+# Copyright 2020-2021 parsa shahmaleki <parsampsh@gmail.com>
+#
+# This file is part of Pashmak.
+#
+# Pashmak is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Pashmak is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Pashmak.  If not, see <https://www.gnu.org/licenses/>.
+#########################################################################
+namespace sys
     namespace path
         func add($args)
             $args = format_args($args)
